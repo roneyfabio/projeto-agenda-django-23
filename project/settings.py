4ab361e8c7f55aa9f578d43622ec8299fb5d3885
Roney Fábio <roneyfabio@gmail.com>
2026-05-19 14:35:20 -0300

    453

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)gw2vs8w)+1+94xk%^o1xgthxvqe#2=ye5^!%x!h=p(c-t0*e'
+SECRET_KEY = 'django-insecure-iz=)z!n+kfr$qu%w*(xk!+fkr8@6n@cq34nf@)tgjamp@vy-va'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+   BASE_DIR / 'base_static',
+)
