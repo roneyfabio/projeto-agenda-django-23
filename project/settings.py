a8c214da0f5302fffe376833fa064820b991585a
Roney Fábio <roneyfabio@gmail.com>
2026-05-22 10:23:32 -0300

    466

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,8 @@ STATIC_ROOT = BASE_DIR / 'static' # collectstatic
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT =  BASE_DIR / 'media' # collectstatic
+
+
+try:
+    from project.local_settings import *
+except: ImportError: ...
