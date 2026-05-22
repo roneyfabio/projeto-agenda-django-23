7a4bd122d6199f348f584577d9f36ef1c9332845
Roney Fábio <roneyfabio@gmail.com>
2026-05-22 13:26:40 -0300

    470

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,7 +4,11 @@ from contact.models import Contact  # noqa: F401
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[:10]
+        
+    print(contacts.query)
     
     context = {
         'contacts': contacts
