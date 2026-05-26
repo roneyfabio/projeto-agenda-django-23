0e841837bf90de659ea57e2d4a68dd6a01e13064
Roney Fábio <roneyfabio@gmail.com>
2026-05-26 08:46:07 -0300

    473

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -12,7 +12,8 @@ def index(request):
     print(contacts.query)
     
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
     
     return render(
@@ -29,8 +30,11 @@ def contact(request, contact_id):
         Contact, pk=contact_id, show=True,        
         )
     
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
     
     return render(
