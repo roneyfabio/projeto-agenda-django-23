562bc296e7ddef39000efda9b524ad9bbd61fd5a
Roney Fábio <roneyfabio@gmail.com>
2026-05-26 12:57:45 -0300

    481

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -5,6 +5,15 @@ from contact.models import Contact  # noqa: F401
 
 
 def create(request):
+    if request.method =='POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()        
+    
+    
+    
     
     context = {}
 
