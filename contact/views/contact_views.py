ba0d49018bea8c803b3e424de4cf31627cc0bf07
Roney Fábio <roneyfabio@gmail.com>
2026-05-26 10:02:28 -0300

    475

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -48,7 +48,8 @@ def search(request):
     
     context = {
         'contacts': contacts,
-        'site_title': 'Search - '
+        'site_title': 'Search - ', 
+        'search_value': search_value,
     }
     
     return render(
