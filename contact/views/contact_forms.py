f81f2df1aac022392f40d4e99609e59b48635e50
Roney Fábio <roneyfabio@gmail.com>
2026-05-26 14:06:08 -0300

    482

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,21 +1,58 @@
-from django.http import Http404  # noqa: F401
-from django.shortcuts import render
+# noqa: F401
+from typing import Any, Dict  # noqa: F401
 
-from contact.models import Contact  # noqa: F401
+from django import forms  # noqa: F401
+from django.core.exceptions import ValidationError  # noqa: F401
+from django.core.paginator import Paginator  # noqa: F401
+from django.db.models import Q  # noqa: F401
+from django.shortcuts import get_object_or_404, redirect, render  # noqa: F401
+
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
 
 
 def create(request):
-    if request.method =='POST':
-        print()
-        print(request.method)
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        print()        
-    
-    
-    
-    
-    context = {}
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
 
     return render(
         request,
