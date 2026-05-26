95887ff4f8f144de99ec19ce5b75f720cbcbb222
Roney Fábio <roneyfabio@gmail.com>
2026-05-26 07:48:18 -0300

    471

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404  # noqa: F401
+from django.shortcuts import get_object_or_404, render
 
 from contact.models import Contact  # noqa: F401
 
@@ -17,5 +18,23 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+    
+    
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True,        
+        )
+    
+    context = {
+        'contact': single_contact,
+    }
+    
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
