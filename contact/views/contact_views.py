fbd8e0585e2135edc9fa6d93f9a4380e426b6882
Roney Fábio <roneyfabio@gmail.com>
2026-05-26 09:37:14 -0300

    474

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django.http import Http404  # noqa: F401
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact  # noqa: F401
 
@@ -21,7 +22,42 @@ def index(request):
         'contact/index.html',
         context
     )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
     
+    if search_value =='':
+        return redirect('contact:index')    
+    
+    print(search_value)    
+    
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(last_name__icontains=search_value)
+            
+            )\
+        .order_by('-id')
+    
+    print(contacts.query)
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
     
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
