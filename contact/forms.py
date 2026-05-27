f5102c2dccc77c670a9d74819b4ef3ceadd6f9e3
Roney Fábio <roneyfabio@gmail.com>
2026-05-27 14:06:24 -0300

    492

--- contact/forms.py
+++ contact/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 
 from . import models
 
 
 class ContactForm(forms.ModelForm):
+
     picture = forms.ImageField(
         widget=forms.FileInput(
             attrs={
@@ -49,3 +51,7 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+class RegisterForm(UserCreationForm):
+    ...
+
