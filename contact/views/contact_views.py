9a6bd6674de5f354980db5e4909f486485f3e095
Roney Fábio <roneyfabio@gmail.com>
2026-05-27 11:17:47 -0300

    490

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -88,3 +88,8 @@ def contact(request, contact_id):
         'contact/contact.html',
         context
     )
+
+    # def delete(request, contact_id):
+    #    contact = get_object_or_404(
+    #     Contact, pk=contact_id, show=True
+    # )
