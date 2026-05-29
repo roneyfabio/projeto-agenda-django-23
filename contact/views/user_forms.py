95707f4489ce9a9d02243e88ebf69cf98034b7a6
Roney Fábio <roneyfabio@gmail.com>
2026-05-29 09:53:06 -0300

    495

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 from contact.forms import RegisterForm
+from django.contrib import auth, messages
 
 
 def register(request):
-    form = RegisterForm()
-
+    form = RegisterForm() 
+     
     if request.method == 'POST':
         form = RegisterForm(request.POST)     
 
         if form.is_valid():
             form.save()
+            messages.success(request,'Usuário registrado')    
+            return redirect('contact:login')
 
 
     return render(
@@ -19,3 +23,28 @@ def register(request):
             'form': form,
         }
     )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request,'Logado com sucesso!')
+            return redirect('contact:index')
+        messages.error(request,'Login inválido')            
+    
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
